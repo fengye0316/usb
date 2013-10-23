e795a5ab26c78dc96255d025c9b2a1512acc839a
artprogramming <ranjunping@163.com>
2013-10-23 23:09:45 +0800

    Create a new file timer.h

---
+++ drivers/timer.h
@@ -0,0 +1,11 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#define TIMER_MODE0	0x00
+#define TIMER_MODE1	0x01
+#define TIMER_MODE2	0x02
+#define TIMER_MODE3	0x03
+
+extern void timer1_init(void);
+
+#endif /* __TIMER_H */
