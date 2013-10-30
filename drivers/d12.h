ba2c8981ac398fcc4c878f886d9b6d36ab724edd
artprogramming <ranjunping@163.com>
2013-10-30 14:22:09 +0800

    Update d12.h

--- drivers/d12.h
+++ drivers/d12.h
@@ -14,8 +14,20 @@
 #define D12_EP2_OUT			4
 #define D12_EP2_IN			5
 
+/* Initialization commands */
+#define D12_CMD_SET_ADDR		0xD0
 
 #define D12_CMD_SET_MODE		0xF3
+#define D12_CMD_SET_DMA			0xFB
+
+/* Data flow commands */
+#define D12_CMD_READ_INTRRUPT		0xF4
+#define D12_INT_EP0_OUT			(1 << 0)
+#define D12_INT_EP0_IN			(1 << 1)
+#define D12_INT_EP1_OUT			(1 << 2)
+#define D12_INT_EP1_IN			(1 << 3)
+#define D12_INT_EP2_OUT			(1 << 4)
+#define D12_INT_EP2_IN			(1 << 5)
 
 extern unsigned short d12_read_id(void);
 
