3289624169d8614f1ddd7facd08698eefd9a5ca2
artprogramming <ranjunping@163.com>
2013-11-01 16:01:17 +0800

    Oh, no... I can't give name for D12..., so removed...

--- drivers/d12.h
+++ drivers/d12.h
@@ -4,8 +4,6 @@
 /* From internet */
 #define D12_CHIP_ID			0x1012
 
-#define D12_CMD_READ_ID			0xFD
-
 
 #define D12_EP0_OUT			0
 #define D12_EP0_IN			1
@@ -14,20 +12,6 @@
 #define D12_EP2_OUT			4
 #define D12_EP2_IN			5
 
-/* Initialization commands */
-#define D12_CMD_SET_ADDR		0xD0
-
-#define D12_CMD_SET_MODE		0xF3
-#define D12_CMD_SET_DMA			0xFB
-
-/* Data flow commands */
-#define D12_CMD_READ_INTRRUPT		0xF4
-#define D12_INT_EP0_OUT			(1 << 0)
-#define D12_INT_EP0_IN			(1 << 1)
-#define D12_INT_EP1_OUT			(1 << 2)
-#define D12_INT_EP1_IN			(1 << 3)
-#define D12_INT_EP2_OUT			(1 << 4)
-#define D12_INT_EP2_IN			(1 << 5)
 
 extern unsigned short d12_read_id(void);
 
