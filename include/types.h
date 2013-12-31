30542572765082e78b416f1124efb64aab166453
artprogramming <ranjunping@163.com>
2013-12-31 17:47:54 +0800

    Create types.h

---
+++ include/types.h
@@ -0,0 +1,22 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+typedef signed char		__s8;
+typedef unsigned char		__u8;
+
+typedef signed short		__s16;
+typedef unsigned short		__u16;
+
+typedef signed int		__s32;
+typedef unsigned int		__u32;
+
+
+typedef __s8			int8_t;
+typedef __s16			int16_t;
+typedef __s32			int32_t;
+
+typedef __u8			uint8_t;
+typedef __u16			uint16_t;
+typedef __u32			uint32_t;
+
+#endif /* __TYPES_H */
